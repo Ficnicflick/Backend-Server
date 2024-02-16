ceb43b61e796f5f563ee8ce6692652b766420ece
BOK-MINJEONG <129048011+BOK-MINJEONG@users.noreply.github.com>
2024-02-16 22:46:14 +0900

    init project

---
+++ src/main/java/com/example/BackendServer/common/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.BackendServer.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    // todo
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilter(this.corsFilter())
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .anyRequest().hasRole("USER")
+                );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring()
+                    .requestMatchers("/signup", "/login");
+        };
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // e.g. http://domain1.com
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ src/main/java/com/example/BackendServer/common/exception/BaseException.java
@@ -0,0 +1,16 @@
+package com.example.BackendServer.common.exception;
+
+import com.example.BackendServer.common.response.BaseResponseStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseException extends RuntimeException {
+    private BaseResponseStatus status;
+
+    public BaseException(BaseResponseStatus status) {
+        super(status.getMessage());
+        this.status = status;
+    }
+}
